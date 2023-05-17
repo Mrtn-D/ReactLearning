5e478da913d48e3f281205b67ba5795fc36d76e6
Martin Daskalov <martin.daskalov@grammasystems.com>
2023-05-17 15:29:42 +0300

    First React Component

---
+++ index.js
@@ -0,0 +1,10 @@
+// https://legacy.reactjs.org/docs/components-and-props.html
+
+class Hello extends React.Component {
+    render(){
+        return <h1>Hello there!</h1>;
+    }
+}
+
+ReactDOM.render(<Hello/>,document.getElementById('root'));
+
