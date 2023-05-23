d988e49769998d053b2e07f9a1ae9f7e93a99fa7
Martin Daskalov <martin.daskalov@grammasystems.com>
2023-05-23 14:13:21 +0300

    Random mood picker

--- index.js
+++ index.js
@@ -1,10 +1,36 @@
-// https://legacy.reactjs.org/docs/components-and-props.html
+// // https://legacy.reactjs.org/docs/components-and-props.html
 
-class Hello extends React.Component {
-    render(){
-        return <h1>Hello there!</h1>;
+// class Hello extends React.Component {
+//     render(){
+//         return <div><h1>Hello there!</h1>
+//         <h1>Hello there!</h1>
+//         <h1>Hello there!</h1>
+//         </div>;
+//     }
+// }
+// // function Hello() {
+// //     return <div>
+// //         <h1>Hello there!</h1>
+// //         <h1>Hello there!</h1>
+// //         <h1>Hello there!</h1>
+// //     </div>;
+// // }
+// ReactDOM.render(<Hello />, document.getElementById('root'));
+
+function getMood(){
+    const moods = ['angry','hungry','silly','happy','sad']
+    return moods [Math.floor(Math.random() * moods.length)];
+}
+
+class JSXDemo extends React.Component {
+    render() {
+        return (
+          <div>
+            <h1>wtf maan: {getMood()}</h1>
+
+          </div>
+        );
     }
 }
 
-ReactDOM.render(<Hello/>,document.getElementById('root'));
-
+ReactDOM.render(<JSXDemo />, document.getElementById('root'))
