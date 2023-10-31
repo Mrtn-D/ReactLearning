be7e7b5dc80a0f9c75d0a7a4227f2bb95ab85d2f
Martin Daskalov <martin.daskalov@grammasystems.com>
2023-10-31 12:35:28 +0200

    Last commit

--- 07_JSX_Loops/Friend.js
+++ 07_JSX_Loops/Friend.js
@@ -1,7 +1,14 @@
 class Friend extends React.Component {
     render() {
+        const { name, hobbies } = this.props;
+        //const lis = hobbies.map(h => <li>{h}</li>)
         return (
-            <p>I AM A FRIEND</p>
+            <div>
+                <h1>{name}</h1>
+                <ul>
+                    {hobbies.map(h => <li>{h}</li>)}
+                </ul>
+            </div>
         )
     }
 }
